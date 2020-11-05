32255004bc50437eef79a246faff0ad0fca382a8
tony inuma <tony.inuma@gmail.com>
2020-11-05 16:17:58 -0500

    Add: instance of List on main class

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById('root'))
+import List from './containers/List'
+
+ReactDOM.render(<List/>, document.getElementById('root'))
