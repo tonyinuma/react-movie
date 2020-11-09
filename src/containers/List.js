1d9e87a4de50fe1c4366513848ad42f88af28894
Tony <tony.inuma@gmail.com>
2020-11-09 00:23:27 -0500

    add: get data from file assets to list and print

--- src/containers/List.js
+++ src/containers/List.js
@@ -1,6 +1,17 @@
 import React from 'react';
-import Card from '../components/Card.js';
 
-const List = () => <Card/>
+class List extends React.Component {
+
+    async componentDidMount(){
+        const res = await fetch('../../assets/data.json');
+        const resJSON = await res.json();
+
+        console.log(resJSON)
+    }
+    
+    render(){
+        return <h1>List</h1>
+    }
+}
 
 export default List;
