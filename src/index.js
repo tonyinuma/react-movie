1fb47cbfd9b7625b84daf296eedb3af40baeea7c
Tony <tony.inuma@gmail.com>
2020-11-09 00:57:28 -0500

    add: import bootstrap

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import ReactDOM from 'react-dom';
 
 import List from './containers/List'
 
+import 'bootswatch/dist/cyborg/bootstrap.min.css';
+
 ReactDOM.render(<List/>, document.getElementById('root'))
