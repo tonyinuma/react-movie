7b9a9fda5b0a82f060f22225aa5041c3fc130df3
tonyID <tony.inuma@gmail.com>
2020-11-25 10:54:29 -0500

    add: module prop types

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                use: ['babel-loader']
+                use: ['babel-loader','eslint-loader']
             },
             {
                 test: /\.css$/,
