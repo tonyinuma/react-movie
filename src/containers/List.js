c0022697d041a63c41e1b04d172f5d0150e8e08a
tonyID <tony.inuma@gmail.com>
2020-11-25 10:15:22 -0500

    Fix: add key to component card

--- src/containers/List.js
+++ src/containers/List.js
@@ -97,8 +97,8 @@ class List extends React.Component {
 
                 <div className="row">
                     {
-                        this.state.data.map(movie => {
-                            return <Card movie={movie}/>
+                        this.state.data.map((movie,i) => {
+                            return <Card movie={movie} key={i}/>
                         })
                     }
                 </div>
